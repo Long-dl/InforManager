b2fd9212dd1981acc77233a6bd7d76fecd154e60
Nhar <toneyhung123@gmail.com>
2021-05-18 12:47:09 +0700

    create

---
+++ InformationManage/src/main/java/com/narga/Information_Manage/InformationManageApplication.java
@@ -0,0 +1,13 @@
+package com.narga.Information_Manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformationManageApplication.class, args);
+	}
+
+}
---
+++ InformationManage/src/test/java/com/narga/Information_Manage/InformationManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narga.Information_Manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
